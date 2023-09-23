44cd5e46f323269aaf88666699705efc1d4b30f0
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-09-23 18:46:24 -0500

    restarted, display menu

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,81 +1,16 @@
-﻿namespace MathGame
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int answer = 0;
-            int score = 0;
+﻿Console.WriteLine("Please type your name");
 
-            Console.WriteLine("Question 1: 5 + 10");
-            answer = Convert.ToInt32(Console.ReadLine());
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
 
-            if (answer == (5 + 10))
-            {
-                Console.WriteLine("Great Job!");
-                score += 1;
-            }
-            else
-            {
-                Console.WriteLine("Not quite...");
-            }
+Console.WriteLine("------------------------------------------------------------------------------------");
 
-            Console.WriteLine("Press Any Key to Continue");
-            Console.ReadKey();
-            Console.Clear();
+Console.WriteLine($"Hello {name.ToUpper()}. It is {date.DayOfWeek}.  Welcome to the Math Game!");
+Console.WriteLine($@"What game would you like to play?
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quite the program");
 
-            Console.WriteLine("Question 2: 18 - 3");
-            answer = Convert.ToInt32(Console.ReadLine());
-
-            if (answer == (18 - 3))
-            {
-                Console.WriteLine("Great Job!");
-                score += 1;
-            }
-            else
-            {
-                Console.WriteLine("Not quite...");
-            }
-
-            Console.WriteLine("Press Any Key to Continue");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("Question 3: 3 * 5");
-            answer = Convert.ToInt32(Console.ReadLine());
-
-            if (answer == (3 * 5))
-            {
-                Console.WriteLine("Great Job!");
-                score += 1;
-            }
-            else
-            {
-                Console.WriteLine("Not quite...");
-            }
-
-            Console.WriteLine("Press Any Key to Continue");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("Question 4: 30 / 2 ");
-            answer = Convert.ToInt32(Console.ReadLine());
-
-            if (answer == (30 / 2))
-            {
-                Console.WriteLine("Great Job!");
-                score += 1;
-            }
-            else
-            {
-                Console.WriteLine("Not quite...");
-            }
-
-            Console.WriteLine("Press Any Key to Continue");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine($"You scored: {score}");
-        }
-    }
-}
+Console.WriteLine("------------------------------------------------------------------------------------");
