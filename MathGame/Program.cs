133e3c50c4c0f480aaca03891c05a6dcc5ee2711
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-09-23 18:10:30 -0500

    create initial game loop

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,7 +4,78 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 0;
+            int score = 0;
+
+            Console.WriteLine("Question 1: 5 + 10");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == (5 + 10))
+            {
+                Console.WriteLine("Great Job!");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Not quite...");
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 2: 18 - 3");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == (18 - 3))
+            {
+                Console.WriteLine("Great Job!");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Not quite...");
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 3: 3 * 5");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == (3 * 5))
+            {
+                Console.WriteLine("Great Job!");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Not quite...");
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 4: 30 / 2 ");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == (30 / 2))
+            {
+                Console.WriteLine("Great Job!");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Not quite...");
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine($"You scored: {score}");
         }
     }
 }
