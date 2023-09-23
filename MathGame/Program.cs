ddcb50a9d87dee53d6e50a634169c56551121ca9
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-09-23 18:54:53 -0500

    collect menu input, display game type

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -14,3 +14,32 @@ D - Division
 Q - Quite the program");
 
 Console.WriteLine("------------------------------------------------------------------------------------");
+
+// Process menu selection
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("Subtraction game selected");
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("Division game selected");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
