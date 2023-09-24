aa92c440098315857026c14c25254ee3de4579af
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-09-23 19:43:38 -0500

    Create game logic

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,4 +1,6 @@
-﻿var date = DateTime.UtcNow;
+﻿using static System.Formats.Asn1.AsnWriter;
+
+var date = DateTime.UtcNow;
 
 string? name = GetName();
 
@@ -55,20 +57,166 @@ Q - Quite the program");
 
 void AdditionGame(string message)
 {
-    Console.WriteLine(message);
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for(int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        // get two random numbers
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        // display two numbers and collect input
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        // check result == sum of the numbers
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
 }
 
 void SubtractionGame(string message)
 {
-    Console.WriteLine(message);
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        // get two random numbers
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        // display two numbers and collect input
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+        var result = Console.ReadLine();
+
+        // check result == sum of the numbers
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
 }
 
 void MultiplicationGame(string message)
 {
-    Console.WriteLine(message);
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        // get two random numbers
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        // display two numbers and collect input
+        Console.WriteLine($"{firstNumber} * {secondNumber}");
+        var result = Console.ReadLine();
+
+        // check result == sum of the numbers
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
 }
 
 void DivisionGame(string message)
 {
-    Console.WriteLine(message);
+    var score = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+
+        var divisionNumbers = GetDivisionNumbers();
+        var firstNumber = divisionNumbers[0];
+        var secondNumber = divisionNumbers[1];
+
+        Console.WriteLine($"{firstNumber} / {secondNumber}");
+        var result = Console.ReadLine();
+
+        // check result == sum of the numbers
+        if (int.Parse(result) == firstNumber / secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
+            score++;
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
+}
+
+int[] GetDivisionNumbers()
+{
+    var random = new Random();
+    var firstNumber = random.Next(1, 99);
+    var secondNumber = random.Next(1, 99);
+
+    var result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
 }
